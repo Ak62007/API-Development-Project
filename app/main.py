4afd008d2fd3735f715193bc056e99b7b5174f27
Aditya Kumar <ied10007.23@bitmesra.ac.in>
2025-06-28 12:32:03 +0530

    resolving venv

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
     
 
 store_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "Favourite Anime", "content": "One Piece", "id": 2}]
