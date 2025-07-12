1fc02107c69fc8b039bc4680a00ec311ab4b2447
Aditya Kumar <ied10007.23@bitmesra.ac.in>
2025-07-12 21:59:22 +0530

    testing

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"messaege": "Hii! my name is Aditya!"}
+    return {"messaege": "Hii! my name is Aditya!!!!!!!"}
 
 # connecting Routes
 app.include_router(posts.router)
