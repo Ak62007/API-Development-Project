16daa81dbe4d270ab1fa9deb3bdd3877cf00216e
Aditya Kumar <ied10007.23@bitmesra.ac.in>
2025-07-30 21:14:34 +0530

    created test database, created tests for create users endpoint testing

---
+++ tests/database.py
@@ -0,0 +1,52 @@
+from fastapi.testclient import TestClient
+import pytest
+from app.main import app
+from app.database import get_db, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+from alembic import command
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+# Create dependency - this must come AFTER SessionLocal is defined
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+app.dependency_overrides[get_db] = override_get_db
+
+@pytest.fixture
+def session():
+    # Run our code before tests
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    # if using alembic (after setting up the revisions)
+    # command.downgrade("base")
+    # command.upgrade("head")
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    # Run our code after tests
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,15 +1,17 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from app import schemas
+from .database import client, session
+    
 
-client = TestClient(app)
-
-response = client.get("/")
-print(response.status_code)
-print(response.json().get("message"))
-
-def test_root():
+def test_root(client):
     response = client.get("/")
     print(response.status_code)
     print(response.json().get("message"))
     assert response.status_code == 200
     assert response.json().get("message") == "hello world!!!!!!!!!!"
+    
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "kkadi8434367970@gmail.com", "password": "adi827193"})
+    user = schemas.UserRes(**res.json())
+    print(user)
+    assert res.status_code == 201
+    
